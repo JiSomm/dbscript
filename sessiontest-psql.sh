ef4f7f4cf137d9d44babbb4594b454cc70de43e6
JiSomm <74135187+JiSomm@users.noreply.github.com>
2021-03-16 11:44:14 +0100

    Update sessiontest-psql.sh

--- sessiontest-psql.sh
+++ sessiontest-psql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if PGPASSWORD=$(vault kv get -tlsskip-verify -field=password secrets/azure/databases/$SERVER) psql -h $SERVER.postgres.database.azure.com -d postgres \
+if PGPASSWORD=$(vault kv get -tls-skip-verify -field=password secrets/azure/databases/$SERVER) psql -h $SERVER.postgres.database.azure.com -d postgres \
 -U $(vault kv get -tls-skip-verify -field=username secrets/azure/databases/$SERVER)"@"$SERVER < ./quit.dbu
 then
   export SERVER=$SERVER
