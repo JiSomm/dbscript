d01dfb54a582a1bc75f8b270de37e5b3c5dce571
JiSomm <74135187+JiSomm@users.noreply.github.com>
2021-03-17 13:16:23 +0100

    Update pushtolaw.py
    python3

--- pushtolaw.py
+++ pushtolaw.py
@@ -1,5 +1,3 @@
-########## PYTHON 2.7 ###########
-
 import json
 import requests
 import datetime
@@ -7,18 +5,15 @@ import hashlib
 import hmac
 import base64
 import os
-import cmd
-from azure.keyvault.secrets import SecretClient
-from azure.identity import DefaultAzureCredential
 
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=os.getenv('VAULT_URL'), credential=credential)
 
 # Update the customer ID to your Log Analytics workspace ID
-customer_id = client.get_secret(os.getenv('LAW_WORKSPACE_ID')).value
+customer_id = client.os.getenv('LAW_WORKSPACE_ID').value
 
 # For the shared key, use either the primary or the secondary Connected Sources client authentication key   
-shared_key = client.get_secret(os.getenv('LAW_PRIMARY_KEY')).value
+shared_key = client.os.getenv('LAW_PRIMARY_KEY').value
 
 # The log type is the name of the event that is being submitted
 log_type = 'DB_logs'
@@ -40,9 +35,9 @@ body = json.dumps(json_data)
 def build_signature(customer_id, shared_key, date, content_length, method, content_type, resource):
     x_headers = 'x-ms-date:' + date
     string_to_hash = method + "\n" + str(content_length) + "\n" + content_type + "\n" + x_headers + "\n" + resource
-    bytes_to_hash = bytes(string_to_hash).encode('utf-8')  
+    bytes_to_hash = bytes(string_to_hash, encoding="utf-8")  
     decoded_key = base64.b64decode(shared_key)
-    encoded_hash = base64.b64encode(hmac.new(decoded_key, bytes_to_hash, digestmod=hashlib.sha256).digest())
+    encoded_hash = base64.b64encode(hmac.new(decoded_key, bytes_to_hash, digestmod=hashlib.sha256).digest()).decode()
     authorization = "SharedKey {}:{}".format(customer_id,encoded_hash)
     return authorization
 
@@ -65,8 +60,8 @@ def post_data(customer_id, shared_key, body, log_type):
 
     response = requests.post(uri,data=body, headers=headers)
     if (response.status_code >= 200 and response.status_code <= 299):
-        print 'Accepted'
+        print('Accepted')
     else:
-        print "Response code: {}".format(response.status_code)
+        print("Response code: {}".format(response.status_code))
 
 post_data(customer_id, shared_key, body, log_type)
