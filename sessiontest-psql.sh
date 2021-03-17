bf03c87bf9f53b6395409ed9d424c934c51ef789
JiSomm <74135187+JiSomm@users.noreply.github.com>
2021-03-17 16:38:55 +0100

    Update sessiontest-psql.sh

--- sessiontest-psql.sh
+++ sessiontest-psql.sh
@@ -3,11 +3,11 @@
 if PGPASSWORD=$(vault kv get -tls-skip-verify -field=password secrets/azure/databases/$SERVER) psql -h $SERVER.postgres.database.azure.com -d postgres \
 -U $(vault kv get -tls-skip-verify -field=username secrets/azure/databases/$SERVER)"@"$SERVER < ./quit.dbu
 then
-  export SERVER=$SERVER
+  export DATABASE=$SERVER
   export STATUS="Active"
   python pushtolaw.py
 else
-  export SERVER=$SERVER
+  export DATABASE=$SERVER
   export STATUS="Error"
   python pushtolaw.py
 fi
