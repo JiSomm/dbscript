31f65d363f7f16e2505749da05e95e1abb5ac22e
JiSomm <74135187+JiSomm@users.noreply.github.com>
2021-03-17 09:22:58 +0100

    Update installPackages.sh

--- installPackages.sh
+++ installPackages.sh
@@ -3,14 +3,15 @@
 apt-get update
 apt-get install curl -y
 apt-get install gnupg -y
-# apt-get install software-properties-common
+apt-get install software-properties-common
 
+# INSTALL PYTHON
 
 # INSTALL HC VAULT
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
-#setcap cap_ipc_lock= /usr/bin/vault
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 apt-get update && apt-get install vault
+setcap cap_ipc_lock= /usr/bin/vault
 
 # INSTALL AZURE CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
