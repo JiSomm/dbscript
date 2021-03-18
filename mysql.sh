c0bf87a62a6cd2d01ea443c08c8aa6763deb256c
JiSomm <74135187+JiSomm@users.noreply.github.com>
2021-03-18 13:57:55 +0100

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-SERVER= 
-USER=${vault kv get -tls-skip-verify -field=username secrets/azure/databases/$SERVER}
-
 > ~/.my.cnf
 
 echo "[client]
 socket=/var/run/mysqld/mysqld.sock
 host=$SERVER.mysql.database.azure.com
-user=$USER@$SERVER
-password=$(vault kv get -tls-skip-verify -field=password secrets/azure/databases/$SERVER)
+user=$(vault kv get -tls-skip-verify --field=username secrets/azure/databases/$SERVER)@$SERVER
+password=$(vault kv get -tls-skip-verify --field=password secrets/azure/databases/$SERVER)
 " >> ~/.my.cnf
 
 if mysql -e quit
